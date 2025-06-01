71d31db877c8758b3ab197143efc45c4dabaadff
dnielclv4free <danielcalvin822@gmail.com>
2025-06-01 20:17:36 +0700

    Update run-bots.sh

--- run-bots.sh
+++ run-bots.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-python main.py --logic Mahal --email=test13@email.com --name=kodokmahal --password=123456 --team etimo &
-python main.py --logic VietCong --email=test42@email.com --name=vietcongg --password=123456 --team etimo &
-python main.py --logic Naive4 --email=test43@email.com --name=naiveeee --password=123456 --team etimo &
-python main.py --logic Putih --email=test34@email.com --name=putihh --password=123456 --team etimo &
+python main.py --logic Garox --email=garox@email.com --name=garox --password=123456 --team etimo &
+python main.py --logic D --email=d42@email.com --name=d --password=123456 --team etimo &
