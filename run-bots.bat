34fb6dab96f4a5a6c6329944d33ab2992c9ce752
dnielclv4free <danielcalvin822@gmail.com>
2025-06-01 20:15:58 +0700

    Add files via upload

---
+++ run-bots.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python src/main.py --logic Mahal --email=test13@email.com --name=kodokmahal --password=123456 --team etimo"
+start cmd /c "python src/main.py --logic VietCong --email=test42@email.com --name=vietcongg --password=123456 --team etimo"
+start cmd /c "python src/main.py --logic Naive4 --email=test43@email.com --name=naiveeee --password=123456 --team etimo"
+start cmd /c "python src/main.py --logic Putih --email=test34@email.com --name=putihh --password=123456 --team etimo"
---
+++ run-bots.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python main.py --logic Mahal --email=test13@email.com --name=kodokmahal --password=123456 --team etimo &
+python main.py --logic VietCong --email=test42@email.com --name=vietcongg --password=123456 --team etimo &
+python main.py --logic Naive4 --email=test43@email.com --name=naiveeee --password=123456 --team etimo &
+python main.py --logic Putih --email=test34@email.com --name=putihh --password=123456 --team etimo &
